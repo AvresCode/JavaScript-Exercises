dcae08ca2330e28dedf7794cc1f00cb8ae5f6c0d
Serva <servas230@gmail.com>
2022-06-13 06:55:37 +0200

    Completed cat walk exercise

--- CatWalk-Async/await/index.js
+++ CatWalk-Async/await/index.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 const STEP_INTERVAL_MS = 50;
 const STEP_SIZE_PX = 10;
 const DANCE_TIME_MS = 5000;
 const DANCING_CAT_URL =
-  'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+  "https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif";
 
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
@@ -19,7 +19,7 @@ function dance(img) {
 }
 
 async function catWalk() {
-  const img = document.querySelector('img');
+  const img = document.querySelector("img");
   const startPos = -img.width;
   const centerPos = (window.innerWidth - img.width) / 2;
   const stopPos = window.innerWidth;
@@ -27,4 +27,4 @@ async function catWalk() {
   // Use async/await syntax to loop the walk and dance functions
 }
 
-window.addEventListener('load', catWalk);
+window.addEventListener("load", catWalk);
